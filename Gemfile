442f5297b8b8d526d67233c59467b7b57b222ad6
kurehajime <xiidec@gmail.com>
2021-11-30 10:07:35 +0000

    gem format

--- Gemfile
+++ Gemfile
@@ -2,7 +2,13 @@
 
 source "https://rubygems.org"
 
-git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
+# Specify your gem's dependencies in kansuji_to_number.gemspec
+gemspec
 
-# gem "rails"
+gem "rake", "~> 13.0"
+
+gem "minitest", "~> 5.0"
+
+gem "rubocop", "~> 1.7"
+
 gem 'parslet', '~> 2.0'
---
+++ lib/kansuji_to_number.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+require_relative "kansuji_to_number/kansuji_nodes"
+require_relative "kansuji_to_number/kansuji_parser"
+require_relative "kansuji_to_number/kansuji_to_number"
+require_relative "kansuji_to_number/kansuji_trans"
+require_relative "kansuji_to_number/version"
+require_relative "kansuji_to_number/version"
+require_relative "kansuji_to_number/version"
+require_relative "kansuji_to_number/version"
+
+module KansujiToNumber
+  class Error < StandardError; end
+  # Your code goes here...
+end
